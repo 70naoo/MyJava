980ba7181f1e3dd60729f7519b8122c76f508b15
naoki <naoki@localhost>
2023-03-29 16:29:23 +0900

    Java 課題 1_10 Practice2 2回目提出

--- HelloWorld/src/practice_for/java__1_10/practice2.java
+++ HelloWorld/src/practice_for/java__1_10/practice2.java
@@ -18,7 +18,7 @@ public class practice2 {
 			System.out.println();
 			
 			for(int j = 1 ; j <= 9; j++) {
-				System.out.println(i + "x" + j + "=" + i*j );
+				System.out.print(i + "x" + j + "=" + i*j + " ");
 			}
 		}
 	}
