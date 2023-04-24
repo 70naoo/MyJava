c2d5b15e2658b9ecd4ab88a33cd18a3cd50aaee7
naoki <naoki@localhost>
2023-04-25 08:04:35 +0900

    java 課題 1_22 再提出

--- JavaProject/src/com/example/processing__1_22/Processor.java
+++ JavaProject/src/com/example/processing__1_22/Processor.java
@@ -15,33 +15,6 @@
 		
   */
 
-//package processing__1_22;
-//import java.time.LocalDateTime;
-//import java.time.format.DateTimeFormatter;
-//
-//public class Processor {
-//    private String location;
-//    private String sushi;
-//    private String foodType;
-//    private LocalDateTime currentDateTime;
-//
-//    public Processor() {
-//        this.location = "日本";
-//        this.sushi = "この寿司はうまい";
-//        this.foodType = "寿司は和食です";
-//        this.currentDateTime = LocalDateTime.now();
-//    }
-//
-//    public void execute() {
-//        String formattedDateTime = this.currentDateTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
-//      
-//        System.out.println("こんにちは！ここは" + this.location + "です！");
-//        System.out.println(this.sushi);
-//        System.out.println(this.foodType);
-//        System.out.println("今の現在日時は" + formattedDateTime + "です");
-//    }
-//}
-
 //パッケージの命名法則
 package com.example.processing__1_22;
 
