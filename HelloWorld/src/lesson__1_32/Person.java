5fba92bf9884b0d7df04aaf5f40c570b62c240ee
naoki <naoki@localhost>
2023-06-12 07:53:41 +0900

    java 課題 lesson32 新規作成

---
+++ HelloWorld/src/lesson__1_32/Person.java
@@ -0,0 +1,53 @@
+package lesson__1_32;
+
+class Person {
+	
+	public String name;
+	
+	public int age;
+	
+    public double height;
+    
+	public double weight;
+	
+	// 問題1：クラスフィールド「count」を定義してください（初期値：0,データ型：int）
+	public static int count = 0;
+
+	Person(String name, int age, double height, double weight) {
+		
+		this.name = name;
+		
+		this.age = age;
+		
+		this.height = height;
+		
+		this.weight = weight;
+		
+		// 問題2：Personコンストラクタの中でクラスフィールドcountに1を足してください
+		count++;
+	}
+
+	public double bmi() {
+		
+		return this.weight / this.height / this.height;
+	}
+
+	public void print(){
+		
+		System.out.println("名前は" + this.name + "です");
+		
+		System.out.println("年は" + this.age + "です");
+	}
+	
+	static int getCount() {
+		
+	    return count;
+	  }
+	
+	// 問題4：クラスメソッド「printCount」を定義してください（データ型：void）
+	// 問題5：クラスメソッドの中にクラスフィールド「count」を用いて「合計〇〇人です」と出力してください
+	public static void printCount() {
+		
+		System.out.println("合計" + count + "人です");
+	}
+}
