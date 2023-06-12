3889ceb03dd8f097c351d6d75728096fa288dba9
naoki <naoki@localhost>
2023-06-12 17:59:55 +0900

    java 課題 lesson32 再提出

--- HelloWorld/src/lesson__1_32/Main.java
+++ HelloWorld/src/lesson__1_32/Main.java
@@ -3,10 +3,16 @@ package lesson__1_32;
 class Main {
 	public static void main(String[] argos) {
 		
+		// インスタンス生成する
 		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+		
+		// メソッドを呼び出す
 		person1.print();
 
+		// インスタンス生成する
 		Person person2 = new Person("山田花子", 22, 1.5, 40);
+		
+		// メソッドを呼び出す
 		person2.print();
 		
 		
--- HelloWorld/src/lesson__1_32/Person.java
+++ HelloWorld/src/lesson__1_32/Person.java
@@ -1,7 +1,7 @@
 package lesson__1_32;
 
 class Person {
-	
+	// インスタンスフィールドを設定する
 	public String name;
 	
 	public int age;
@@ -13,6 +13,7 @@ class Person {
 	// 問題1：クラスフィールド「count」を定義してください（初期値：0,データ型：int）
 	public static int count = 0;
 
+	// コンストラクタを定義する
 	Person(String name, int age, double height, double weight) {
 		
 		this.name = name;
@@ -27,11 +28,13 @@ class Person {
 		count++;
 	}
 
+	// bmiメソッドを作成する
 	public double bmi() {
 		
 		return this.weight / this.height / this.height;
 	}
 
+	// printメソッドを作成する
 	public void print(){
 		
 		System.out.println("名前は" + this.name + "です");
@@ -39,6 +42,7 @@ class Person {
 		System.out.println("年は" + this.age + "です");
 	}
 	
+	// getCountメソッドを作成する
 	static int getCount() {
 		
 	    return count;
@@ -46,6 +50,8 @@ class Person {
 	
 	// 問題4：クラスメソッド「printCount」を定義してください（データ型：void）
 	// 問題5：クラスメソッドの中にクラスフィールド「count」を用いて「合計〇〇人です」と出力してください
+	
+	// printCountメソッドを作成する
 	public static void printCount() {
 		
 		System.out.println("合計" + count + "人です");
